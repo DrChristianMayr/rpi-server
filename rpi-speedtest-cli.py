bd160a60c9f81137c3e5bdb32d91317872d3453c
ChrisTRIan <chriz@me.com>
2020-12-31 17:15:55 +0100

    Update rpi-speedtest-cli.py

--- rpi-speedtest-cli.py
+++ rpi-speedtest-cli.py
@@ -2,11 +2,25 @@ import os
 import re
 import subprocess
 import time
+from influxdb import InfluxDBClient
 
-testinterval=5 #
+testinterval = 5 #
+writeCSV = false
+writeInfluxDB = false
+
 print('start')
 try:
-    testinterval=os.environ['TEST_INTERVAL']
+    testinterval = os.environ['TEST_INTERVAL']
+except:
+    pass
+
+try:
+    writeCSV = os.environ['WRITE_CSV']
+except:
+    pass
+
+try:
+    writeInfluxDB = os.environ['WRITE_INFLUXDB']
 except:
     pass
 
@@ -20,15 +34,36 @@ ping = ping[0].replace(',', '.')
 download = download[0].replace(',', '.')
 upload = upload[0].replace(',', '.')
 
-try:
-    f = open('./speedtest.csv', 'a+')
-    if os.stat('./speedtest.csv').st_size == 0:
-            f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
-except:
-    pass
+if writeCSV
+    try:
+        f = open('./speedtest.csv', 'a+')
+        if os.stat('./speedtest.csv').st_size == 0:
+                f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
+    except:
+        pass
 
-f.write('{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, download, upload))
-print(time.strftime('%m/%d/%y') + ' ' + time.strftime('%H:%M') + ' Ping: ' + str(ping) + ' ms, Download: ' + str(download) + ' Mbit/s Upload: '+ str(upload) + ' Mbit/s')
+    f.write('{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, download, upload))
+
+if writeInfluxDB
+    speed_data = [
+        {
+            "measurement" : "internet_speed",
+            "tags" : {
+                "host": "127.0.0.1"
+            },
+            "fields" : {
+                "download": float(download),
+                "upload": float(upload),
+                "ping": float(ping)
+            }
+        }
+    ]
+    client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'pimylifeup', 'internetspeed')
+    client.write_points(speed_data)
+
+
+
+print(time.strftime('%m/%d/%y') + ', ' + time.strftime('%H:%M') + ' Ping: ' + str(ping) + ' ms, Download: ' + str(download) + ' Mbit/s Upload: '+ str(upload) + ' Mbit/s')
 print('Waiting for ' + str(testinterval) + ' seconds')
 time.sleep(testinterval) 
 print('end')
