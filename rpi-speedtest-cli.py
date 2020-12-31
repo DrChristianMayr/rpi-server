8b1d6c008ddc28302299573f4746903b53969046
ChrisTRIan <chriz@me.com>
2020-12-31 16:08:27 +0100

    Create rpi-speedtest-cli.py

---
+++ rpi-speedtest-cli.py
@@ -0,0 +1,25 @@
+import os
+import re
+import subprocess
+import time
+
+echo 'start'
+response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+
+ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
+download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
+upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
+
+ping = ping[0].replace(',', '.')
+download = download[0].replace(',', '.')
+upload = upload[0].replace(',', '.')
+
+try:
+    f = open('/home/pi/speedtest/speedtest.csv', 'a+')
+    if os.stat('/home/pi/speedtest/speedtest.csv').st_size == 0:
+            f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
+except:
+    pass
+
+f.write('{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, download, upload))
+echo 'end'
