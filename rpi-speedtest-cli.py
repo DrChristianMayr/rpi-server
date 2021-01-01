9ba62d1b4a035a2df685730dad9d8efaee12c45f
ChrisTRIan <chriz@me.com>
2021-01-01 23:02:52 +0100

    Update rpi-speedtest-cli.py

--- rpi-speedtest-cli.py
+++ rpi-speedtest-cli.py
@@ -6,6 +6,71 @@ import time
 import argparse
 from influxdb import InfluxDBClient
 
+def db_exists():
+    '''returns True if the database exists'''
+    dbs = client.get_list_database()
+    for db in dbs:
+        if db['name'] == dbname:
+            return True
+    return False
+
+def wait_for_server(host, port, nretries=5):
+    '''wait for the server to come online for waiting_time, nretries times.'''
+    url = 'http://{}:{}'.format(host, port)
+    waiting_time = 1
+    for i in range(nretries):
+        try:
+            requests.get(url)
+            return 
+        except requests.exceptions.ConnectionError:
+            print('waiting for', url)
+            time.sleep(waiting_time)
+            waiting_time *= 2
+            pass
+    print('cannot connect to', url)
+    sys.exit(1)
+
+def connect_db(host, port, reset):
+    '''connect to the database, and create it if it does not exist'''
+    global client
+    print('connecting to database: {}:{}'.format(host,port))
+    client = InfluxDBClient(host, port, retries=5, timeout=1)
+    wait_for_server(host, port)
+    create = False
+    if not db_exists():
+        create = True
+        print('creating database...')
+        client.create_database(dbname)
+    else:
+        print('database already exists')
+    client.switch_database(dbname)
+    if not create and reset:
+        client.delete_series(measurement=measurement)
+        
+def measure(nmeas):
+    '''insert dummy measurements to the db.
+    nmeas = 0 means : insert measurements forever. 
+    '''
+    i = 0
+    if nmeas==0:
+        nmeas = sys.maxsize
+    for i in range(nmeas):
+        x = i/10.
+        y = math.sin(x)
+        data = [{
+            'measurement':measurement,
+            'time':datetime.datetime.now(),
+            'tags': {
+                'x' : x
+                },
+                'fields' : {
+                    'y' : y
+                    },
+            }]
+        client.write_points(data)
+        pprint.pprint(data)
+        time.sleep(1)
+        
 def get_module_logger(mod_name):
     """
     To use this, do logger = get_module_logger(__name__)
