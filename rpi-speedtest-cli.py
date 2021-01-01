4d63422f6673953566ebd1da7dcb8e9543443803
ChrisTRIan <chriz@me.com>
2021-01-01 22:17:07 +0100

    Update rpi-speedtest-cli.py

--- rpi-speedtest-cli.py
+++ rpi-speedtest-cli.py
@@ -10,13 +10,14 @@ def get_module_logger(mod_name):
     """
     To use this, do logger = get_module_logger(__name__)
     """
-    logger = logging.getLogger(mod_name)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
+    if not my_logger.handlers:
+        logger = logging.getLogger(mod_name)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
     return logger
 
 def str2bool(v):
