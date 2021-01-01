1bc09f980c678adea7f3a93ba6df6ad42adcf8cb
ChrisTRIan <chriz@me.com>
2021-01-01 21:44:20 +0100

    Update rpi-speedtest-cli.py

--- rpi-speedtest-cli.py
+++ rpi-speedtest-cli.py
@@ -1,10 +1,23 @@
 import os
-import sys
+import logging
 import re
 import subprocess
 import time
 import argparse
 from influxdb import InfluxDBClient
+
+def get_module_logger(mod_name):
+    """
+    To use this, do logger = get_module_logger(__name__)
+    """
+    logger = logging.getLogger(mod_name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -20,9 +33,11 @@ def str2bool(v):
 testinterval = 10 #
 writeCSV = False
 writeInfluxDB = False
-
+__name__ = 'rpi-speedtest-cli'
+    
 # Initiate the parser
 parser = argparse.ArgumentParser()
+
 # Add long and short argument
 parser.add_argument("--testinterval", "-t", help="set testinveral in s. [default = 300s (5 minutes)]", type=int)
 parser.add_argument("--writeCSV", "-c", type=str2bool, nargs='?', const=True, default=writeCSV, help="set True/False to write CSV file. [default = True]")
@@ -30,7 +45,7 @@ parser.add_argument("--writeInfluxDB", "-i", type=str2bool, nargs='?', const=Tru
 
 # Read arguments from the command line
 args = parser.parse_args()
-print(args)
+get_module_logger(__name__).info(args)
 
 # Check for --testinterval
 if args.testinterval:
@@ -48,7 +63,7 @@ if args.writeInfluxDB:
     writeInfluxDB=args.writeInfluxDB
 
 # conduct speedtest
-sys.stdout('conduct speedtest')
+get_module_logger(__name__).info("conduct speedtest")
 response = subprocess.Popen('speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 #response = subprocess.Popen('speedtest-cli', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
